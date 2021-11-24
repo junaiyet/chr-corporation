44215d23c678054be40a02d1cad6b1edeffe510a
junaiyet <junaiyetmia470@gmail.com>
2021-11-24 18:07:04 +0600

    crr-corpooration

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,14 +24,51 @@ $('.hero-slider').slick({
 
 });
 // ///hero-slider end///////
+// ///projects-slider start///////
 $('.projects-slider').slick({
   dots: true,
   arrows:false,
-  prevArrow:'<i class="fas fa-chevron-left arrows  prevarrow"></i>',
-  nextArrow:'<i class="fas fa-chevron-right arrows  nextarrow"></i>',
+
   infinite: true,
   speed: 300,
   slidesToShow: 2,
   slidesToScroll: 2,
 
 });
+// ///projects-slider end///////
+// ///news-slider start///////
+
+$('.news-slider').slick({
+  dots: true,
+  arrows:false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+
+});
+// ///news-slider end///////
+// testimonial-slider start////
+$('.testimonial-slider').slick({
+  dots: true,
+  arrows:false,
+
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+
+});
+// testimonial-slider end////
+// testimonial-slider start////
+$('.clients-slider').slick({
+  dots: false,
+  arrows:false,
+
+  infinite: true,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+
+});
+// testimonial-slider end////
